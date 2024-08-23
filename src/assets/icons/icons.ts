ff1eac2e98d35208ecb162528eb4905df16674bb
Blankeos <carloantonioct@gmail.com>
2024-08-24 02:00:12 +0800

    feat: Added icons.

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -4,4 +4,10 @@
 // - Custom icons (.svg) can be added in this folder and exported thanks to vite-plugin-solid-svg.
 // - Get your icons from https://icones.js.org/
 // ===========================================================================
-export { default as IconLogo } from "./logo.svg";
+export { default as IconChevronRight } from './chevron-right.svg';
+export { default as IconClipboard } from './clipboard.svg';
+export { default as IconCopy } from './copy.svg';
+export { default as IconLogo } from './logo.svg';
+export { default as IconPaintBrush } from './paint-brush.svg';
+export { default as IconScissors } from './scissors.svg';
+export { default as IconSizes } from './sizes.svg';
